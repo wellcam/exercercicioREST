a27f9b1ccfbd12c0c8ea19ac40b7b69aeee5079f
Wellington Camilo <wellington.camilo@discover.com.br>
2020-03-17 15:43:52 -0300

    rm ResponseBody add RestController - indicando que o rertono sera sempre de dados | Criançao de DTO pra enxugar informações retornadas | Converter stream Object to DTO

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,23 +3,22 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDTO;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
+	public List<TopicoDTO> lista() {
 		
 		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
 		
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDTO.convert(Arrays.asList(topico, topico, topico));
 	}
 	
 }
