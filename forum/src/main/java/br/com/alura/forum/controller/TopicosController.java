6ab6b42d70cb77ed889f0bf31a0f67563832ec39
Wellington Camilo <wellington.camilo@discover.com.br>
2020-03-18 12:06:22 -0300

    Criando Repository para metodos DAO | Removendo construtores

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,24 +1,26 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDTO;
-import br.com.alura.forum.model.Curso;
+import br.com.alura.forum.controller.repository.TopicoRepository;
 import br.com.alura.forum.model.Topico;
 
 @RestController
 public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
 
 	@RequestMapping("/topicos")
 	public List<TopicoDTO> lista() {
 		
-		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-		
-		return TopicoDTO.convert(Arrays.asList(topico, topico, topico));
+		List <Topico> topicos = topicoRepository.findAll();
+		return TopicoDTO.convert(topicos);
 	}
 	
 }
